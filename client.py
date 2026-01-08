3fe3c86ffb75caed78f783f4df1bbb790e07ec47
Roman Yakimov <{ID}+{username}@users.noreply.github.com>
2026-01-08 13:30:22 +0100

    Add get_person.

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
 import requests
 import json
 
-from constants import BASE_URL, GET_PROFILE, GET_BIO, DEFAULT_KEY
-
+from constants import BASE_URL, GET_PROFILE, GET_PERSON, GET_BIO, DEFAULT_KEY
 
 
 def get_profile(key: str = DEFAULT_KEY, base_url: str = BASE_URL, timeout: int = 10):
@@ -10,6 +9,18 @@ def get_profile(key: str = DEFAULT_KEY, base_url: str = BASE_URL, timeout: int =
     params = {
     'action': GET_PROFILE,
     'key': key
+    }
+
+    response = requests.get(base_url, params=params, timeout=timeout)
+    response.raise_for_status()
+
+    return response.json()
+
+def get_person(key: str = DEFAULT_KEY, base_url: str = BASE_URL, timeout: int = 10):
+
+    params = {
+        'action': GET_PERSON,
+        'key': key
     }
 
     response = requests.get(base_url, params=params, timeout=timeout)
